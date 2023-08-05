3d50232c7a580a0943305ffca7e0c261d7858cfa
Pavel <apdilwibim@gmail.com>
2023-08-05 12:48:37 +0300

    fix calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,11 +2,7 @@ import getRandomNubmer from '../utils.js';
 import genMainLogic from '../index.js';
 
 const description = 'What is the result of the expression?';
-
-const getRandomOperation = () => {
-  const arr = ['-', '+', '*'];
-  return arr[getRandomNubmer(0, 2)];
-};
+const operations = ['-', '+', '*'];
 
 const getResultOfExpression = (a, operation, b) => {
   let result = 0;
@@ -30,7 +26,7 @@ const genOneRound = () => {
   const result = [];
   const num1 = getRandomNubmer(1, 100);
   const num2 = getRandomNubmer(1, 100);
-  const operation = getRandomOperation();
+  const operation = operations[getRandomNubmer(0, 2)];
   const expression = `${num1} ${operation} ${num2}`;
   const rightAnswer = getResultOfExpression(num1, operation, num2);
   result.push(expression, String(rightAnswer));
