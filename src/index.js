07b64cc2e76a2f71b2e755e9f07f17cac570c29c
Pavel <apdilwibim@gmail.com>
2023-08-05 12:39:05 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -10,13 +10,12 @@ export default (description, genOneRound) => {
   for (let i = 0; i < iterationsCount; i += 1) {
     const [expression, rightAnswer] = genOneRound();
     const answerOfUser = readlineSync.question(`Question: ${expression}\nAnswer: `);
-    if (rightAnswer === answerOfUser.toLowerCase()) {
-      console.log('Correct!');
-    } else {
+    if (rightAnswer !== answerOfUser.toLowerCase()) {
       console.log(`'${answerOfUser}' is wrong answer ;(. Correct answer was '${rightAnswer}.'`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
 };
