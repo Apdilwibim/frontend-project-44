5c13c7bfbe9acaecf0782850c00fb68388769ecb
Pavel <apdilwibim@gmail.com>
2023-08-05 13:07:56 +0300

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,8 +1,8 @@
 import getRandomNubmer from '../utils.js';
 import genMainLogic from '../index.js';
 
-const isEven = (number) => number % 2 === 0;
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = (number) => number % 2 === 0;
 
 const genOneRound = () => {
   const randomNumber = getRandomNubmer(1, 100);
