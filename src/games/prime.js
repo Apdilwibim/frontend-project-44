5a4e7e60594c7e3390a9fbf63aa0200ef1899926
Pavel <apdilwibim@gmail.com>
2023-08-09 15:33:08 +0300

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,10 +4,11 @@ import runGame from '../index.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
+  const limit = Math.sqrt(Number(num));
   if (num <= 1) {
     return false;
   }
-  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i += 1) {
+  for (let i = 2; i <= limit; i += 1) {
     if (num % i === 0) {
       return false;
     }
@@ -16,10 +17,9 @@ const isPrime = (num) => {
 };
 
 const genOneRound = () => {
-  const num = getRandomNubmer(1, 100);
-  const expression = String(num);
-  const rightAnswer = isPrime(num) ? 'yes' : 'no';
-  return [expression, rightAnswer];
+  const number = String(getRandomNubmer(1, 100));
+  const rightAnswer = isPrime(number) ? 'yes' : 'no';
+  return [number, rightAnswer];
 };
 
 export default () => {
