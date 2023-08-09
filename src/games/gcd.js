ba214d61e5a204affa79129ebf8f49f8849d0ac6
Pavel <apdilwibim@gmail.com>
2023-08-09 15:25:55 +0300

    fix gcd function

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,19 +3,11 @@ import runGame from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const getGcd = (number1, number2) => {
-  let temp1 = number1;
-  let temp2 = number2;
-  let result = 0;
-  while (temp1 !== temp2) {
-    if (temp1 > temp2) {
-      temp1 -= temp2;
-    } else {
-      temp2 -= temp1;
-    }
+const getGcd = (a, b) => {
+  if (!b) {
+    return a;
   }
-  result = temp1;
-  return result;
+  return getGcd(b, a % b);
 };
 
 const genOneRound = () => {
